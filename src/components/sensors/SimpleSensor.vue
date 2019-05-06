cdf30dcd475f334745f5c3323cc04308d6c34274
Aleksander Giera <aleksander.giera@protonmail.com>
2019-05-06 08:25:47 +0200

    added button to expand more info

--- src/components/sensors/SimpleSensor.vue
+++ src/components/sensors/SimpleSensor.vue
@@ -3,29 +3,36 @@
     <v-card class="ma-1 text-md-center align-center" min-width="700px">
       <v-toolbar color="primary" dark>
         <v-toolbar-title>{{sensorDisplay.sensor_id}}</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn @click="changeVisiblity()" icon>
+          <v-icon v-if="visible">expand_less</v-icon>
+          <v-icon v-else>expand_more</v-icon>
+        </v-btn>
       </v-toolbar>
-      <v-layout row class="pa-2">
-        <v-flex lg12>
-          <div class="caption grey--text">Platform</div>
-          <div>{{sensorDisplay.platform}}</div>
-        </v-flex>
-      </v-layout>
-      <v-layout v-for="(item , index) in sensorDisplay.metrics" :key="index" class="pa-2">
-        <v-flex lg6>
-          <div class="caption grey--text">Metric</div>
-          <div>{{item.metric}}</div>
-        </v-flex>
+      <v-conatiner v-if="visible">
+        <v-layout row class="pa-2">
+          <v-flex lg12>
+            <div class="caption grey--text">Platform</div>
+            <div>{{sensorDisplay.platform}}</div>
+          </v-flex>
+        </v-layout>
+        <v-layout v-for="(item , index) in sensorDisplay.metrics" :key="index" class="pa-2">
+          <v-flex lg6>
+            <div class="caption grey--text">Metric</div>
+            <div>{{item.metric}}</div>
+          </v-flex>
 
-        <v-flex lg2>
-          <div class="caption grey--text">Unit</div>
-          <div>{{item.unit}}</div>
-        </v-flex>
+          <v-flex lg2>
+            <div class="caption grey--text">Unit</div>
+            <div>{{item.unit}}</div>
+          </v-flex>
 
-        <v-flex lg4>
-          <div class="caption grey--text">Timestamp</div>
-          <div>{{item.timestamp}}</div>
-        </v-flex>
-      </v-layout>
+          <v-flex lg4>
+            <div class="caption grey--text">Timestamp</div>
+            <div>{{item.timestamp}}</div>
+          </v-flex>
+        </v-layout>
+      </v-conatiner>
     </v-card>
   </v-layout>
 </template>
@@ -38,10 +45,21 @@ export default {
       required: true
     }
   },
+  data() {
+    return {
+      visible: false
+    };
+  },
 
   computed: {
     sensorDisplay() {
       return this.sensor;
+    }
+  },
+
+  methods: {
+    changeVisiblity() {
+      this.visible = !this.visible;
     }
   }
 };
